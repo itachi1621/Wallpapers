8e9ab6586020d3b6ded22e13026c6d1bbaa50e4c
ZERO <zero0396@gmail.com>
2020-12-14 12:34:00 -0400

    First Upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Wallpapers
+### Wallpapers
+My collection
+
+[![*](https://raw.githubusercontent.com/itachi1621/Wallpapers/master/thumbnails/*)](https://raw.githubusercontent.com/itachi1621/Wallpapers/master/wallpaper/*)
---
+++ make_gallery.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# set -e
+
+# Usage: ./make_gallery.sh
+#
+# Run in a directory with a "papes/" subdirectory, and it will create a
+# "thumbnails/" subdirectory.
+#
+# Uses imagemagick's `convert`, so make sure that's installed.
+# On Nix, nix-shell -p imagemagick --run ./make_gallery.sh
+
+rm -rf thumbnails
+mkdir thumbnails
+
+url_root="https://raw.githubusercontent.com/itachi1621/Wallpapers/master"
+
+echo "### Wallpapers" >README.md
+echo "My collection" >>README.md
+echo "" >>README.md
+
+total=$(ls papes/ | wc -l)
+i=0
+
+for src in papes/*; do
+  ((i++))
+  filename="$(basename "$src")"
+  printf '%4d/%d: %s\n' "$i" "$total" "$filename"
+
+  convert -resize 200x "$src" "${src/wallpaper/thumbnails}"
+
+  filename_escaped="${filename// /%20}"
+  thumb_url="$url_root/thumbnails/$filename_escaped"
+  pape_url="$url_root/wallpaper/$filename_escaped"
+
+  echo "[![$filename]($thumb_url)]($pape_url)" >>README.md
+done
